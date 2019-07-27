b363785fabc6b226972fcceb1dd928de63ffae4b
naikvaibhav <naikvaibhav1994@gmail.com>
2019-07-27 10:16:42 +0530

    update so that only the words of a string are reversed and not the whole string

--- src/app/reverse-str.pipe.ts
+++ src/app/reverse-str.pipe.ts
@@ -3,6 +3,6 @@ import { Pipe, PipeTransform } from '@angular/core';
 @Pipe({ name: 'reverseStr' })
 export class ReverseStr implements PipeTransform {
     transform(value: string): string {
-        return value.split('').reverse().join('');
+       return value.split('').reverse().join('').split(" ").reverse().join(" ");
     }
 }
